18de5d3f3974aa699c4dddc7feeee1ddea36cdb9
kubrasnmez <sonmezkbra@gmail.com>
2021-05-05 23:25:37 +0300

    05.05

---
+++ nLayeredDemo/src/nLayeredDemo/business/concretes/ProductManager.java
@@ -0,0 +1,36 @@
+package nLayeredDemo.business.concretes;
+
+import java.util.List;
+
+import nLayeredDemo.business.abstracts.ProductService;
+import nLayeredDemo.dataAccess.abstracts.ProductDao;
+import nLayeredDemo.entities.concretes.Product;
+
+public class ProductManager implements ProductService {
+	
+	public ProductManager(ProductDao productDao) {
+		super();
+		this.productDao = productDao;
+	}
+
+	private ProductDao productDao;
+	
+
+	@Override
+	public void add(Product product) {
+		if(product.getCategoryId()==1) {
+			System.out.println("Bu kategoride ürün kabul edilemiyor");
+			return ;
+		}
+		this.productDao.add(product);
+		
+		
+	}
+
+	@Override
+	public List<Product> getAll() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
