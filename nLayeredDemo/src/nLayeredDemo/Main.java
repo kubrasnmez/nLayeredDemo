ae7784cc82398cf855808bbb949878431ab9e6a7
kubrasnmez <sonmezkbra@gmail.com>
2021-05-06 00:09:53 +0300

    logger

--- nLayeredDemo/src/nLayeredDemo/Main.java
+++ nLayeredDemo/src/nLayeredDemo/Main.java
@@ -2,6 +2,7 @@ package nLayeredDemo;
 
 import nLayeredDemo.business.abstracts.ProductService;
 import nLayeredDemo.business.concretes.ProductManager;
+import nLayeredDemo.core.JLoggerManagerAdapter;
 import nLayeredDemo.dataAccess.concretes.HibernateProductDao;
 import nLayeredDemo.entities.concretes.Product;
 
@@ -9,7 +10,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		//ToDo Spring IoC ile çözülecek
-		ProductService productService = new ProductManager(new HibernateProductDao());
+		ProductService productService = new ProductManager(new HibernateProductDao(),new JLoggerManagerAdapter());
 		
 		Product product = new Product(1,2,"Elma",12,50);
 		productService.add(product);
